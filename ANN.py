0cfac61e6ce5cf62c1ceb209a1aa052cdd36f00e
Sotiroulla Konstantinou <skonstan@cern.ch>
2019-10-25 22:37:32 +0200

    updated files

--- ANN.py
+++ ANN.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #uproot: https://www.indico.shef.ac.uk/event/11/contributions/338/attachments/281/319/rootTutorialWeek5_markhod_2018.pdf
 import keras
 import uproot
@@ -18,10 +19,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.externals import joblib
 import keras.backend as K
 from keras.optimizers import SGD
-
-import plot
 from sklearn.metrics import roc_auc_score
 
+import plot
+import func
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #Disable AVX/FMA Warning
 #Default values:
 LAMBDA = 1
 NEPOCHS = 200
@@ -32,35 +36,18 @@ parser.add_option("--lam",dest="lam",  default=LAMBDA,  type = int,   help="Lamb
 parser.add_option("--nepochs", dest="nepochs", default = NEPOCHS, type = int, help ="Number of epochs (Default: %s)" % NEPOCHS)
 opt, args = parser.parse_args()
 
-def getDirName():
-    dirName = "TopTag"
-    dirName = dirName.replace(".", "p")
-    dirName = "/afs/cern.ch/user/s/skonstan/public/html/"+dirName
-    return dirName
 
-def SavePlot(canvas, saveDir, saveName):
-    savePath = "%s/%s" % (saveDir, saveName)
-    saveURL  = savePath.replace("/afs/cern.ch/user/s/","https://cmsdoc.cern.ch/~")
-    saveURL  = saveURL.replace("/public/html/","/")
-    canvas.SaveAs(savePath)
-    savePath = savePath.replace("pdf","root")
-    canvas.SaveAs(savePath)
-    print "=== ", saveURL
-    return
+###########################################################
+# Plot Loss Function
+###########################################################
 
 def PlotLossFunction(losses, nepochs, saveDir):
     ROOT.gStyle.SetOptStat(0)
     canvas = ROOT.TCanvas("canvas", "canvas",0,0,800,800)
     canvas.cd()
-    #hLf    = ROOT.TH1F('Lf', '', nepochs, 0, nepochs)
-    #hLr    = ROOT.TH1F('Lr', '', nepochs, 0, nepochs)
-    #hLfr    = ROOT.TH1F('Lfr', '', nepochs, 0, nepochs)
     xarr = []
     for i in range(1, nepochs+1):
         xarr.append(i)
-    #    hLf.Fill(i,losses["L_f"][i])
-    #    hLr.Fill(i,losses["L_r"][i])
-    #    hLfr.Fill(i,losses["L_f - L_r"][i])
 
     hLf  = ROOT.TGraph(len(xarr), array('d', xarr), array('d', losses["L_f"]))
     hLr  = ROOT.TGraph(len(xarr), array('d', xarr), array('d', losses["L_r"]))
@@ -70,8 +57,6 @@ def PlotLossFunction(losses, nepochs, saveDir):
     ymin = min(hLf.GetHistogram().GetMinimum(), hLr.GetHistogram().GetMinimum(), hLfr.GetHistogram().GetMinimum())
     ymax = ymax + 1
     ymin = ymin - 1
-    print "========== MAX:", ymax
-    print "========== MIN:", ymin
 
     hLf.SetLineColor(ROOT.kRed)
     hLf.SetMarkerColor(ROOT.kRed)
@@ -112,198 +97,7 @@ def PlotLossFunction(losses, nepochs, saveDir):
     #canvas.SetLogy()
 
     saveName = "Loss_lam%s.pdf" %(opt.lam)
-    SavePlot(canvas, saveDir, saveName)
-    canvas.Close()
-    return
-
-def PlotOvertrainingTest(Y_train_S, Y_test_S, Y_train_B, Y_test_B, saveDir, saveName):
-    ROOT.gStyle.SetOptStat(0)
-    canvas = ROOT.TCanvas("canvas", "canvas",0,0,800,800)
-    canvas.cd()
-    canvas.SetLogy()
-
-    hList = []
-    DataList = [Y_train_S, Y_test_S, Y_train_B, Y_test_B]
-    ymax = 0
-    nbins = 500
-    htrain_s=ROOT.TH1F('train_s', '', nbins, 0.0, 1.)
-    hList.append(htrain_s)
-    htest_s=ROOT.TH1F('test_s', '', nbins, 0.0, 1.)
-    hList.append(htest_s)
-    htrain_b=ROOT.TH1F('train_b', '', nbins, 0.0, 1.)
-    hList.append(htrain_b)
-    htest_b=ROOT.TH1F('test_b', '', nbins, 0.0, 1.)
-    hList.append(htest_b)
-            
-    for i in range(len(DataList)):
-        for r in DataList[i]:
-            hList[i].Fill(r)        
-        ymax = max(ymax, hList[i].GetMaximum())
-    
-    htrain_s1 = htrain_s.Clone("train_s")
-    htrain_b1 = htrain_b.Clone("train_b")
-    htest_s1  = htest_s.Clone("test_s")
-    htest_b1  = htest_b.Clone("test_b")
-
-    drawStyle = "HIST SAME"
-    leg=plot.CreateLegend(0.50, 0.65, 0.85, 0.85)    
-    for h in hList:
-        h.SetMaximum(ymax*2)        
-        h.GetXaxis().SetTitle("Output")
-        h.GetYaxis().SetTitle("Entries")
-        h.Rebin(10)
-        # Legend
-        legText, legStyle = plot.GetLegendStyle(h.GetName())
-        leg.AddEntry(h, legText, legStyle)
-        
-        plot.ApplyStyle(h)
-        h.Draw(plot.DrawStyle(h.GetName())+" SAME")
-
-    graph = plot.CreateGraph([0, 0], [0, ymax*1.1])
-    #graph.Draw("same")
-    leg.Draw()
-
-    SavePlot(canvas, saveDir, saveName)
-    canvas.Close()
-    return htrain_s1, htest_s1, htrain_b1, htest_b1
-
-def PlotEfficiency(htest_s, htest_b, saveDir, saveName):
-    ROOT.gStyle.SetOptStat(0)
-    canvas = ROOT.TCanvas("canvas", "canvas",0,0,800,800)
-    canvas.cd()
-    nbins = htest_s.GetNbinsX()
-    # Initialize sigma variables
-    sigmaAll = ROOT.Double(0.0)
-    sigmaSel = ROOT.Double(0.0)
-
-    All_s = htest_s.IntegralAndError(0, nbins+1, sigmaAll, "")
-    All_b = htest_b.IntegralAndError(0, nbins+1, sigmaAll, "")
-
-    eff_s = []; eff_b = []; xvalue = []; error = []
-    for i in range(0, nbins+1):
-        Sel_s = htest_s.IntegralAndError(i, nbins+1, sigmaSel, "")
-        Sel_b = htest_b.IntegralAndError(i, nbins+1, sigmaSel, "")
-
-        if (All_s <= 0):
-            All_s = 1
-            Sel_s = 0
-        if (All_b <= 0):
-            All_b = 1
-            Sel_b = 0
-
-        eff_s.append(Sel_s/All_s)
-        eff_b.append(Sel_b/All_b)
-        error.append(0)
-        xvalue.append(htest_s.GetBinCenter(i))
-        
-    graph_s = plot.GetGraph(xvalue, eff_s, error, error, error, error)
-    graph_b = plot.GetGraph(xvalue, eff_b, error, error, error, error)
-
-    graph_s.SetLineColor(ROOT.kBlue)
-    graph_s.SetMarkerColor(ROOT.kBlue)
-    graph_s.SetMarkerStyle(8)
-    graph_s.SetMarkerSize(0.4)
-    graph_b.SetLineColor(ROOT.kRed)
-    graph_b.SetMarkerColor(ROOT.kRed)
-    graph_b.SetMarkerStyle(8)
-    graph_b.SetMarkerSize(0.4)
-
-    graph_s.SetMinimum(0)
-    graph_b.SetMinimum(0)
-    
-    #
-    h_signif0=ROOT.TH1F('signif0', '', nbins, 0.0, 1.)
-    h_signif1=ROOT.TH1F('signif1', '', nbins, 0.0, 1.)
-
-    sign = []
-    sigmaSel_s = ROOT.Double(0.0)
-    sigmaSel_b = ROOT.Double(0.0)
-
-    for i in range(0, nbins+1):
-        # Get selected events                                                                                                                                                                       
-        sSel = htest_s.IntegralAndError(i, nbins+1, sigmaSel_s, "")
-        bSel = htest_b.IntegralAndError(i, nbins+1, sigmaSel_b, "")
-        # Calculate Significance                                                                                                                                                                    
-        _sign0 = sSel/math.sqrt(sSel+bSel)
-        _sign1 = 2*(math.sqrt(sSel+bSel) - math.sqrt(bSel))
-        sign.append(_sign0)
-        h_signif0.Fill(htest_s.GetBinCenter(i), _sign0)        
-        h_signif1.Fill(htest_s.GetBinCenter(i), _sign1)        
-
-    graph_signif = plot.GetGraph(xvalue, sign, error, error, error, error)
-    graph_signif.SetLineColor(ROOT.kGreen)
-    graph_signif.SetMarkerColor(ROOT.kGreen)
-    graph_signif.SetMarkerStyle(8)
-    graph_signif.SetMarkerSize(0.4)
-
-    h_signif0.SetLineColor(ROOT.kGreen)
-    h_signif0.SetMarkerColor(ROOT.kGreen)
-    h_signif0.SetLineWidth(3)
-
-    h_signif1.SetLineColor(ROOT.kGreen+3)
-    h_signif1.SetMarkerColor(ROOT.kGreen+3)
-    h_signif1.SetLineWidth(3)
-    
-    #=== Scale Significance
-    maxSignif0 = h_signif0.GetMaximum()
-    maxSignif1 = h_signif1.GetMaximum()
-    maxSignif = max(maxSignif0, maxSignif1)
-
-    graph_s.SetTitle("")
-    graph_b.SetTitle("")
-
-    
-    h_signifScaled0 = h_signif0.Clone("signif0")
-    h_signifScaled0.Scale(1./float(maxSignif))
-
-    h_signifScaled1 = h_signif1.Clone("signif1")
-    h_signifScaled1.Scale(1./float(maxSignif))
-
-    ymax = max(h_signifScaled0.GetMaximum(), h_signifScaled1.GetMaximum())
-    graph_s.SetMaximum(ymax*1.1)
-    graph_b.SetMaximum(ymax*1.1)
-    h_signifScaled0.SetMaximum(ymax*1.1)
-    h_signifScaled1.SetMaximum(ymax*1.1)
-
-    graph_s.SetMinimum(0)
-    graph_b.SetMinimum(0)
-    h_signifScaled0.SetMinimum(0)
-    h_signifScaled1.SetMinimum(0)
-
-    graph_s.GetXaxis().SetTitle("Output")
-    graph_s.GetYaxis().SetTitle("Efficiency")    
-    graph_b.GetXaxis().SetTitle("Output")
-    graph_b.GetYaxis().SetTitle("Efficiency")    
-
-    h_signifScaled0.GetXaxis().SetTitle("Output")
-    h_signifScaled0.GetYaxis().SetTitle("Efficiency")
-    h_signifScaled1.GetXaxis().SetTitle("Output")
-    h_signifScaled1.GetYaxis().SetTitle("Efficiency")
-    #Draw
-    h_signifScaled0.Draw("HIST")
-    h_signifScaled1.Draw("HIST SAME")
-    graph_s.Draw("P SAME")
-    graph_b.Draw("P SAME")
-
-    #Legend
-    leg=plot.CreateLegend(0.50, 0.25, 0.85, 0.45)    
-    leg.AddEntry(graph_s, "Signal Efficiency", "l")
-    leg.AddEntry(graph_b, "Bkg Efficiency", "l")
-    leg.AddEntry(h_signifScaled0, "S/#sqrt{S+B}", "l")
-    leg.AddEntry(h_signifScaled1, "2#times(#sqrt{S+B} - #sqrt{B})", "l")
-    leg.Draw()
-    # Define Right Axis (Significance)
-    signifColor = ROOT.kGreen+2
-    rightAxis = ROOT.TGaxis(1, 0, 1, 1.1, 0, 1.1*maxSignif, 510, "+L")
-    #rightAxis.SetWmax(1.1*maxSignif)
-    rightAxis.SetLineColor ( signifColor )
-    rightAxis.SetLabelColor( signifColor )
-    rightAxis.SetTitleColor( signifColor )
-    rightAxis.SetTitleOffset(1.3)
-    rightAxis.SetTitle( "Significance" )
-    rightAxis.Draw()
-        
-    SavePlot(canvas, saveDir, saveName)
+    plot.SavePlot(canvas, saveDir, saveName)
     canvas.Close()
     return
 
@@ -514,7 +308,7 @@ roc_auc_score(Y_test, y_pred)
 for i in range(50):
     print("Predicted=%s (%s) (%s)"% (target_test[i], y_pred[i], Y_test[i]))
 
-saveDir = getDirName()
+saveDir = plot.getDirName("TopTag")
 PlotLossFunction(losses, nepochs, saveDir)
 
 
