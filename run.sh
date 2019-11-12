a78e6628bd29a0937d98a077b8fef9b4ec7667f9
Alexandros Attikis <attikis@cern.ch>
2019-11-12 10:57:55 +0100

    [sequential]: run script update

--- run.sh
+++ run.sh
@@ -16,14 +16,63 @@
 # 
 # sleep 10
 
-#for bs in 100 200 500 1000; do 
-for bs in 16 32 64 128 256 512 1024; do 
-    echo "=== Epochs = ${ep}, BatchSize = ${bs}"
-    sleep 15
-    #nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,200,1 --epochs 100 --batchSize $bs --log -s pdf &
-    #nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,500,1 --epochs 1000 --batchSize $bs --log -s pdf &
-    nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,100,1 --epochs 1000 --batchSize $bs --log -s pdf &
+# v0
+# for bs in 16 32 64 128 256 512 1024; do 
+#     echo "=== Epochs = ${ep}, BatchSize = ${bs}"
+#     sleep 15
+#     #nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,200,1 --epochs 100 --batchSize $bs --log -s pdf &
+#     #nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,500,1 --epochs 1000 --batchSize $bs --log -s pdf &
+#     #nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,100,1 --epochs 1000 --batchSize $bs --log -s pdf &
+#     #nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,200,1 --epochs 1000 --batchSize $bs --log -s pdf &
+#     nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,500,1 --epochs 1000 --batchSize $bs --log -s pdf &
+# done
 
-    #nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,500,1 --epochs 100 --batchSize $bs --log -s pdf &
-    #nohup ./sequential.py --activation elu,elu,sigmoid --neurons 19,500,1 --epochs 100 --batchSize $bs --log -s pdf &
+# v1
+# for n in 19 20 50 100 500 1000 5000; do 
+#     echo "=== Neurons = ${n}"
+#     sleep 15
+#     nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,$n,1 --epochs 1000 --batchSize 64 --log -s pdf &
+# done
+
+# v2
+# for bs in 16 64 128 256 1000 50000 100000; do
+#     echo "=== BatchSize = ${bs}"
+#     sleep 15
+#     #nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,1000,1 --epochs 1000 --batchSize ${bs} --log -s pdf &
+#     nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,1000,1 --epochs 1000 --batchSize ${bs} --log -s pdf &
+# done
+
+# v3
+# for bs in 40 50 60 64 70 80 90 100 ; do
+#     echo "=== BatchSize = ${bs}"
+#     sleep 15
+#     nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,1000,1 --epochs 1000 --batchSize ${bs} --log -s pdf &
+# done
+
+# v4
+# for bs in 1 5 10 20 40; do
+#     echo "=== BatchSize = ${bs}"
+#     sleep 15
+#     nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,1000,1 --epochs 1000 --batchSize ${bs} --log -s pdf &
+# done
+
+# v5
+# nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,500,1 --epochs 1000 --batchSize 32 --log -s pdf &
+# sleep 15
+# nohup ./sequential.py --activation relu,relu,relu,sigmoid --neurons 19,500,250,1 --epochs 1000 --batchSize 32 --log -s pdf &
+# sleep 15
+# nohup ./sequential.py --activation relu,relu,relu,relu,sigmoid --neurons 19,500,250,125,1 --epochs 1000 --batchSize 32 --log -s pdf &
+# sleep 15
+# nohup ./sequential.py --activation relu,relu,relu,relu,relu,sigmoid --neurons 19,500,250,125,60,1 --epochs 1000 --batchSize 32 --log -s pdf &
+# sleep 15
+# nohup ./sequential.py --activation relu,relu,relu,relu,relu,relu,sigmoid --neurons 19,500,250,125,60,30,1 --epochs 1000 --batchSize 32 --log -s pdf &
+# sleep 15
+# nohup ./sequential.py --activation relu,relu,relu,relu,relu,relu,relu,sigmoid --neurons 19,500,250,125,60,30,19,1 --epochs 1000 --batchSize 32 --log -s pdf &
+
+# v6
+for n in 19 19*2 19*3 19*5 19*10 19*100; do
+    echo "=== Neurons = ${n}"
+    sleep 15
+    nohup ./sequential.py --activation relu,relu,sigmoid --neurons 19,${n},1 --epochs 1000 --batchSize 32 --log -s pdf &
 done
+
